b6cecdf75e81e639d0dbd2bb48a0668800b8d804
unkleho <kahoc@phm.gov.au>
2018-01-15 12:10:18 +1100

    Updated Readme. added peerDeps.

---
+++ README.md
@@ -0,0 +1,4 @@
+# ESLint Config for DX Lab
+
+## Notes
+15/1/18 - Can't enable `no-tabs` because it causes `Error: Aborted because 17 is not accepted` error. Switched prettier to use no tabs too.
--- index.js
+++ index.js
@@ -30,12 +30,11 @@ module.exports = {
     "consistent-return": 0,
     "dot-notation": 0,
     "import/prefer-default-export": 0,
-    "indent": 0,
+    // "indent": 0,
     "lines-around-directive": 0,
     "max-len": 0,
     "no-console": 0,
     "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
-    "no-tabs": 0,
     "no-unused-vars": [2, { "argsIgnorePattern": "^_" }],
     "no-use-before-define": 0,
     "padded-blocks": ["error", { "classes": "always" }]
